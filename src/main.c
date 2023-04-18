74b618e6cedc261da7559730e54c06bc8b55d554
Martin <martnluque1798@gmail.com>
2023-04-18 11:00:00 -0300

    Parto desde el TP 2

---
+++ src/main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "alumno.h"
+
+int main(void) {
+
+    static const struct alumno_s yo = {
+        .apellido = "Lopez Luque",
+        .nombre = "Martin Eduardo",
+        .documento = 41143177,
+    };
+    char cadena[128];
+
+    /**
+     * @brief Pruebo comentar archivo .c
+     *
+     */
+    if (serializar(&yo, cadena, sizeof(cadena)) >= 0) {
+        printf("%s\n", cadena);
+    } else {
+        printf("error al serializar\n");
+    }
+
+    return 0;
+}
